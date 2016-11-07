bed0e4fb02dfdd4af50f2b639bc81efde3717333
seul88 <erwin.majewski@gmail.com>
2016-11-07 22:22:33 +0100

    gotMessage() and connectToServer() methods

--- Telnet/src/main/java/com/majstu/client/Client.java
+++ Telnet/src/main/java/com/majstu/client/Client.java
@@ -1,6 +1,77 @@
 package com.majstu.client;
 
-public class Client {
-}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-// brach testowy...
+
+public class Client {
+
+	 private BufferedReader in;
+	 private PrintWriter out;
+		
+	 // todo:	graphical user interface
+	
+	 String message;
+	 String addressIP;
+	 
+	 
+	public Client(){
+	
+		gotMessage();
+	
+		
+	}
+	
+	
+	
+	
+	
+	
+	public void gotMessage() {
+	
+		try {
+            message = in.readLine();
+            if (message == null || message.equals("")) {
+                  System.exit(0);
+              }
+        } catch (IOException ex) {
+               message = "Error: " + ex;
+        }
+		
+	}
+
+
+
+
+
+
+	public void connectToServer() throws IOException {
+		
+		System.out.println("Podaj adres IP serwera");
+		addressIP = in.readLine();
+        Socket socket = new Socket(addressIP, 992);
+
+        
+        in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+
+		
+		
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) throws Exception {
+		
+		Client client = new Client();
+		
+		 client.connectToServer();
+	}
+	
+}
